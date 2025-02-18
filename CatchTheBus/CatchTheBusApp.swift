386d295d42c6ccd668a2ae90d29dca5357d1026d
Artem Horovenko <amolon210@icloud.com>
2025-02-18 10:20:14 +0100

    Initial Commit

---
+++ CatchTheBus/CatchTheBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheBusApp.swift
+//  CatchTheBus
+//
+//  Created by amolonus on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
