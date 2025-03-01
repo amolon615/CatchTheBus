a7964c14ccfb06937c2938c5c2d478c308fe40ec
Artem Horovenko <amolon210@icloud.com>
2025-03-01 22:51:50 +0000

    Fixed path for fetchTrips request

--- CatchTheBus/Shared/Utils/Constants.swift
+++ CatchTheBus/Shared/Utils/Constants.swift
@@ -9,10 +9,11 @@
 struct Constants {
     static let baseURL: String = "https://api.ember.to/v1"
     
-    static func getAllTripsInfoUR(fromDate from: String, toDate to: String) -> String { "quotes/?origin=13&destination=42&departure_date_from=\(from)&departure_date_to=\(to)"
+    static func getAllTripsInfoUR(fromDate from: String, toDate to: String) -> String { "/quotes/?origin=13&destination=42&departure_date_from=\(from)&departure_date_to=\(to)"
     }
     
     static func getOneTripDetailedInfoURL(withID tripdID: String) -> String {
         return "/trips/\(tripdID)/"
     }
+    
 }
