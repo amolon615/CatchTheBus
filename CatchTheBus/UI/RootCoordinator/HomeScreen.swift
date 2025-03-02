c3d9b7ccb2a300e463a38c426ab35e013e036293
Artem Horovenko <amolon210@icloud.com>
2025-03-02 23:20:53 +0000

    Added save for current trip

--- CatchTheBus/UI/RootCoordinator/HomeScreen.swift
+++ CatchTheBus/UI/RootCoordinator/HomeScreen.swift
@@ -10,11 +10,25 @@ import SwiftUI
 final class UIRootCoordinatorViewModel: ObservableObject {
     @Published private(set) var state: RootViewState = .allTrips
     
+    @AppStorage("savedCurrentTripUID") var currentTrackedTripUID: String?
+    
+    init() {
+        loadSavedTrackedTrip()
+    }
+    
+    func loadSavedTrackedTrip() {
+        if let currentTrackedTripUID = currentTrackedTripUID {
+            trackTrip(currentTrackedTripUID)
+        }
+    }
+    
     func trackTrip(_ tripUID: String) {
+        currentTrackedTripUID = tripUID
         state = .trackedTrip(tripUID)
     }
     
     func untrackTrip() {
+        currentTrackedTripUID = nil
         state = .allTrips
     }
 }
